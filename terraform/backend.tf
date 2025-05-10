a27b973a4eeca9e5b533708a230a8d231c2ecd7b
fastII <anderson.oliveira1979@gmail.com>
2025-05-10 11:00:57 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "desafio-us-east-1-terraform"
-    key            = "terraform/desafioasaptech/terraform.tfstate"
+    key            = "ec2/minikube/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-dynamo"
     encrypt        = true
