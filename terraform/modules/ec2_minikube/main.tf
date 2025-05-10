62bc8594e711bf5c9fedf374f922e083323d8bdc
fastII <anderson.oliveira1979@gmail.com>
2025-05-10 11:20:12 -0300

    Update main.tf

--- terraform/modules/ec2_minikube/main.tf
+++ terraform/modules/ec2_minikube/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "minikube" {
   connection {
     type        = "ssh"
     user        = "ec2-user" # ou "ubuntu"
-    private_key = private_key = file(var.private_key_path)
+    private_key = file(var.private_key_path)
     host        = self.public_ip
   }
 
