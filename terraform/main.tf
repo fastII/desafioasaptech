8d02f3175e1d193d8d13cbb08c91d251e691fe2e
fastii <anderson.oliveira1979@gmail.com>
2025-05-12 11:42:01 -0300

    add estrutura helmchart

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,19 @@ module "ec2_minikube" {
   private_key_path  = var.private_key_path
   ami_id          = var.ami_id
 }
+
+resource "null_resource" "upload_helm_chart" {
+  provisioner "file" {
+    source      = "helm-chart"
+    destination = "/home/ec2-user/chart"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.private_key_path)
+      host        = module.ec2_minikube.public_ip
+    }
+  }
+
+  depends_on = [module.ec2_minikube]
+}
