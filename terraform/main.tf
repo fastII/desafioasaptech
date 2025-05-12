ea7d6a3385afc745da4865ef29d3ee4452f4f677
fastii <anderson.oliveira1979@gmail.com>
2025-05-12 12:40:50 -0300

    add estrutura helmchart bug

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ module "ec2_minikube" {
 
 resource "null_resource" "upload_helm_chart" {
   provisioner "file" {
-    source      = "../helm-chart"
+    source      = "helm-chart"
     destination = "/home/ec2-user/chart"
 
     connection {
