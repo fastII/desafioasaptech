79663d7c880608a8006e48809461fb5014f23dd4
fastii <anderson.oliveira1979@gmail.com>
2025-05-22 13:16:38 -0300

    exclusão variaveis

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,8 +24,5 @@ variable "ami_id" {
 
 variable "environment" {}
 variable "aws_region" {}
-variable "vpc_cidr" {}
-variable "azs" { type = list(string) }
-variable "public_subnets" { type = list(string) }
-variable "private_subnets" { type = list(string) }
+
 
